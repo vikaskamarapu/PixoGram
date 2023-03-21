62427c798a97ccc2d430c5917415f622a6c6ef9e
Vikas Kamarapu <vikaskamarapu2001@gmail.com>
2023-03-22 00:26:21 +0530

    pagination completed version 2

--- components/home/Post.js
+++ components/home/Post.js
@@ -42,11 +42,11 @@ const styles = StyleSheet.create({
     }
 })
 
-const Post = ({ item }) => {
+const Post = ({ post }) => {
 
-    const handleLike = async (item) => {
-        const currentLikesStatus = !item.likesByUsers.includes(auth.currentUser.email)
-        const postRef = doc(db, `users/${item.owner_email}/posts/${item.id}`);
+    const handleLike = async (post) => {
+        const currentLikesStatus = !post?.likesByUsers.includes(auth.currentUser.email)
+        const postRef = doc(db, `users/${post?.owner_email}/posts/${post?.id}`);
         try {
             currentLikesStatus ?
                 await updateDoc(postRef, {
@@ -64,18 +64,18 @@ const Post = ({ item }) => {
 
     return (
         <View style={{ marginBottom: 10 }}>
-            <PostHeader item={item} />
-            <PostImage item={item} handleLike={handleLike} />
-            <PostFooter item={item} handleLike={handleLike} />
-            <Likes item={item} />
-            <Caption item={item} />
-            <CommentsSection item={item} />
-            <Comments item={item} />
+            <PostHeader post={post} />
+            <PostImage post={post} handleLike={handleLike} />
+            <PostFooter post={post} handleLike={handleLike} />
+            <Likes post={post} />
+            <Caption post={post} />
+            <CommentsSection post={post} />
+            <Comments post={post} />
         </View>
     );
 }
 
-const PostHeader = ({ item }) => {
+const PostHeader = ({ post }) => {
     return <View style={{
         flexDirection: 'row',
         justifyContent: 'space-between',
@@ -86,7 +86,7 @@ const PostHeader = ({ item }) => {
         <View style={{ flexDirection: "row" }}>
             <LinearGradient colors={['#CA1D7E', '#E35157', '#F2703F']} style={styles.storyBorder}>
                 <View style={styles.innerBorder}>
-                    <Image source={{ uri: item.profile_picture }} style={styles.story} />
+                    <Image source={{ uri: post?.profile_picture }} style={styles.story} />
                 </View>
             </LinearGradient>
             <Text style={{
@@ -95,7 +95,7 @@ const PostHeader = ({ item }) => {
                 marginLeft: 4,
                 fontWeight: '600',
                 fontSize: 15,
-            }}>{item.user}</Text>
+            }}>{post?.user}</Text>
         </View>
         <TouchableOpacity>
             <Image style={styles.icon} source={{ uri: "https://img.icons8.com/tiny-glyph/16/ffffff/experimental-menu-2-tiny-glyph.png" }} />
@@ -103,29 +103,29 @@ const PostHeader = ({ item }) => {
     </View>
 }
 
-const PostImage = ({ item, handleLike }) => {
+const PostImage = ({ post, handleLike }) => {
     const win = Dimensions.get('window');
     return (
-        <TouchableOpacity onPress={() => handleLike(item)} activeOpacity={1} >
+        <TouchableOpacity onPress={() => handleLike(post)} activeOpacity={1} >
             <View style={{
                 width: win.width,
-                height: win.width+150,
+                height: win.width + 150,
             }} >
-                <Image source={{ uri: item.imageUrl }} style={{ height: '100%', resizeMode: 'cover' }} />
+                <Image source={{ uri: post?.imageUrl }} style={{ height: '100%', resizeMode: 'cover' }} />
             </View>
         </TouchableOpacity>
-    )  
+    )
 }
 
-const PostFooter = ({ item, handleLike }) => (
+const PostFooter = ({ post, handleLike }) => (
     <View style={{
         flexDirection: 'row',
         marginVertical: 8,
         justifyContent: 'space-between',
     }}>
         <View style={{ flexDirection: 'row' }}>
-            <TouchableOpacity onPressIn={() => { handleLike(item) }}>
-                <Image source={!item.likesByUsers.includes(auth.currentUser.email) ? require('../../assets/images/heart.png') : require('../../assets/images/redHeart.png')} style={styles.footerIcon} />
+            <TouchableOpacity onPressIn={() => { handleLike(post) }}>
+                <Image source={!post?.likesByUsers.includes(auth.currentUser.email) ? require('../../assets/images/heart.png') : require('../../assets/images/redHeart.png')} style={styles.footerIcon} />
             </TouchableOpacity>
             <TouchableOpacity>
                 <Image source={require('../../assets/images/chat.png')} style={styles.footerIcon} />
@@ -140,39 +140,39 @@ const PostFooter = ({ item, handleLike }) => (
     </View>
 )
 
-const Likes = ({ item }) => (
+const Likes = ({ post }) => (
     <View style={{ flexDirection: 'row', }}>
-        <Text style={{ color: 'white', fontWeight: '600', marginLeft: 10 }}>{item.likesByUsers.length.toString().replace(/(\d)(?=(\d\d)+\d$)/g, "$1,")} likes</Text>
+        <Text style={{ color: 'white', fontWeight: '600', marginLeft: 10 }}>{post?.likesByUsers.length.toString().replace(/(\d)(?=(\d\d)+\d$)/g, "$1,")} likes</Text>
     </View>
 )
 
-const Caption = ({ item }) => (
+const Caption = ({ post }) => (
     <View>
         <Text style={{ color: 'white', marginLeft: 10, marginTop: 4, }}>
-            <Text style={{ fontWeight: '700' }}>{item.user}</Text>
-            <Text> {item.caption}</Text>
+            <Text style={{ fontWeight: '700' }}>{post?.user}</Text>
+            <Text> {post?.caption}</Text>
         </Text>
     </View>
 )
 
-const CommentsSection = ({ item }) => (
+const CommentsSection = ({ post }) => (
     <View style={{ marginLeft: 10, marginTop: 5 }}>
         <TouchableOpacity>
-            {!!item.comments.length && (
+            {!!post?.comments.length && (
                 <Text style={{ color: 'gray', }}>
                     View
-                    {item.comments.length > 1 ? ' all ' : ' '}
-                    {item.comments.length}
-                    {item.comments.length > 1 ? ' comments' : ' comment'}
+                    {post?.comments.length > 1 ? ' all ' : ' '}
+                    {post?.comments.length}
+                    {post?.comments.length > 1 ? ' comments' : ' comment'}
                 </Text>
             )}
         </TouchableOpacity>
     </View>
 )
 
-const Comments = ({ item }) => (
+const Comments = ({ post }) => (
     <>
-        {item.comments.map((comment, index) => (
+        {post?.comments.map((comment, index) => (
             <View key={index}>
                 <Text style={{ color: 'white', marginLeft: 10, marginTop: 4, }} >
                     <Text style={{ fontWeight: '700' }}>{comment.user}</Text>
--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,11 +1,11 @@
-import { View, Text, StyleSheet, Platform, StatusBar, ActivityIndicator } from 'react-native'
+import { View, Text, StyleSheet, Platform, StatusBar, ActivityIndicator, ScrollView } from 'react-native'
 import React, { useEffect, useState } from 'react'
 import Header from '../components/home/Header'
 import Stories from '../components/home/Stories'
 import Post from '../components/home/Post'
 import { POSTS } from '../Data/Posts'
 import BottomTabs from '../components/home/BottomTabs'
-import { collection, getDocs, collectionGroup, query, where, limit, orderBy, onSnapshot, startAfter } from "firebase/firestore";
+import { collection, getDocs, collectionGroup, query, where, limit, orderBy, onSnapshot, startAfter, getCountFromServer } from "firebase/firestore";
 import { auth, db } from '../firebase'
 import { FlatList } from 'react-native'
 
@@ -21,7 +21,9 @@ export default function HomeScreen({ navigation }) {
   const [posts, setPosts] = useState([]);
   const [currentUser, setCurrentUser] = useState([]);
   const [lastDoc, setLastDoc] = useState();
-  const [page, setPage] = useState(1);
+  const [page, setPage] = useState(0);
+  const [postsCount, setPostsCount] = useState(0);
+  const [endOfPage, setEndOfPage] = useState(false);
 
   useEffect(() => {
     onSnapshot(query(collection(db, "users"), where("email", "==", auth.currentUser.email), limit(1)), (doc) => {
@@ -35,6 +37,10 @@ export default function HomeScreen({ navigation }) {
 
   useEffect(() => {
     if (posts.length === 0) {
+      const q = query(collectionGroup(db, "posts"));
+      getCountFromServer(q).then((value) => {
+        setPostsCount(value.data().count);
+      });
       onSnapshot(query(collectionGroup(db, "posts"), orderBy('createdAt', 'desc'), limit(2)), (doc) => {
         const received = [];
         setLastDoc(doc.docs[doc.docs.length - 1]);
@@ -42,42 +48,70 @@ export default function HomeScreen({ navigation }) {
           received.push({ id: doc.id, ...doc.data() });
         });
         setPosts(received)
+        console.log(received);
       });
     }
     else {
-      onSnapshot(query(collectionGroup(db, "posts"), orderBy('createdAt', 'desc'), startAfter(lastDoc), limit(2)), (doc) => {
-        const received = [];
-        setLastDoc(doc.docs[doc.docs.length - 1]);
-        doc.forEach((doc) => {
-          received.push({ id: doc.id, ...doc.data() });
+      if (posts.length < postsCount) {
+        onSnapshot(query(collectionGroup(db, "posts"), orderBy('createdAt', 'desc'), startAfter(lastDoc), limit(2)), (doc) => {
+          const received = [];
+          setLastDoc(doc.docs[doc.docs.length - 1]);
+          doc.forEach((doc) => {
+            received.push({ id: doc.id, ...doc.data() });
+          });
+          setPosts([...posts, ...received])
+          console.log(received);
         });
-        setPosts([...posts, ...received])
-      });
+      }
     }
   }, [page]);
-  
+
   const getMore = () => {
-    setPage(page + 1);
+    if (!endOfPage)
+      setPage(page + 2);
   }
 
   const Foot = () => (
     <ActivityIndicator size='large' color={"grey"} />
   )
 
+  function isCloseToBottom({ layoutMeasurement, contentOffset, contentSize }) {
+    return layoutMeasurement.height + contentOffset.y >= contentSize.height - 20;
+  }
+
+  function isCloseToTop({ layoutMeasurement, contentOffset, contentSize }) {
+    return contentOffset.y == 0;
+  }
+
   return <View style={styles.container}>
     <Header navigation={navigation} />
-    {/* <ScrollView showsVerticalScrollIndicator={false} style={{ marginBottom: 45 }} onScrollEndDrag={getMore}>
+    <ScrollView
+      showsVerticalScrollIndicator={false}
+      style={{ marginBottom: 60 }}
+      onScroll={({ nativeEvent }) => {
+        if (isCloseToTop(nativeEvent)) {
+          //do something
+        }
+        if (isCloseToBottom(nativeEvent)) {
+          //do something
+          getMore();
+          if (posts.length >= postsCount) {
+            setEndOfPage(true);
+          }
+        }
+      }}
+    >
       <Stories />
-      <Divider width={1} orientation='vertical' style={{ marginVertical: 10 }} />
       {posts ?
         posts.map((post, index) => (
           <Post post={post} key={index} />
         )) :
         'Loading....'
       }
-    </ScrollView> */}
-    <FlatList
-      data={posts}
+      {!endOfPage && <Foot />}
+    </ScrollView>
+    {/* <FlatList
+      data={posts} 
       renderItem={Post}
       ListHeaderComponent={Stories}
       onEndReached={getMore}
@@ -85,7 +119,7 @@ export default function HomeScreen({ navigation }) {
       keyExtractor={posts => (posts.id)}
       ListFooterComponent={Foot}
       style={{ marginBottom: 50 }}
-    />
+    /> */}
     {currentUser ? <BottomTabs navigation={navigation} currentUser={currentUser} /> : "Loading...."}
   </View>
 }
